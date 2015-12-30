3da69f76cb2d2837e0cf4d6cd1976c3967d6f945
Dan Berger <dberger@aaas.org>
2015-12-29 17:45:15 -0700

    some updates to get started locally

---
+++ docroot/sites/default/files/php/twig/f95408e7_input.html.twig_ecc6da207a237bb59fa4c55858fdb7b68cc46d887987a3735a5c6630f96db036/f219bba2c1a70e9d0ff00dda0903ae56bd7b5979d324179d2a4b55528af40d5a.php
@@ -0,0 +1,55 @@
+<?php
+
+/* core/themes/classy/templates/form/input.html.twig */
+class __TwigTemplate_a06a9c961dd11b15871d0e37d85d0790bcaf68d7b6505f0d79ceae53b0d0d8dc extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 13
+        echo "<input";
+        echo $this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["attributes"]) ? $context["attributes"] : null), "html", null, true);
+        echo " />";
+        echo $this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["children"]) ? $context["children"] : null), "html", null, true);
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/form/input.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 13,);
+    }
+}
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Theme override for an 'input' #type form element.*/
+/*  **/
+/*  * Available variables:*/
+/*  * - attributes: A list of HTML attributes for the input element.*/
+/*  * - children: Optional additional rendered elements.*/
+/*  **/
+/*  * @see template_preprocess_input()*/
+/*  *//* */
+/* #}*/
+/* <input{{ attributes }} />{{ children }}*/
+/* */
